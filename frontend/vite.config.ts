859528aa04f5d00c4c40375fa4420496fd100f4e
Alexandre Estapé Carnicer <alexandre.estape@gmail.com>
2025-10-02 15:41:49 +0200

    hotfix mime

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/3482390483957.github.io/",
+  base: "/test/",
   plugins: [react()],
 });
